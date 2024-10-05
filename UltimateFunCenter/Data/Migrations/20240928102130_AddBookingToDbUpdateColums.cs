7d019527bb441c489504587111933376937ea906
volocarucatalin <volocaru.catalin17@gmail.com>
2024-10-05 12:07:46 +0100

    Update on the booking model

---
+++ UltimateFunCenter/Data/Migrations/20240928102130_AddBookingToDbUpdateColums.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UltimateFunCenter.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBookingToDbUpdateColums : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Bookings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "HowManyPeople",
+                table: "Bookings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "HowMayMinors",
+                table: "Bookings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "HowManyPeople",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "HowMayMinors",
+                table: "Bookings");
+        }
+    }
+}
